3eb7cd043adb0fb1af4f28369b33fec65812430c
Pablo Solar Vilariño <psolarvi@redhat.com>
2021-06-03 13:50:31 +0200

    ge(ch02s08): Added plain Java producer with plain viewer

---
+++ kafka-producers/plain/producer/src/main/java/com/redhat/telemetry/producer/ProducerApp.java
@@ -0,0 +1,37 @@
+package com.redhat.telemetry.producer;
+
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SslConfigs;
+
+public class ProducerApp {
+    public static Properties configureProperties() {
+        Properties props = new Properties();
+
+        // @todo: configure the bootstrap server
+
+        // @todo: configure the key and value serializers
+
+        // @todo: configure the SSL connection
+
+        return props;
+    }
+
+    public static void main(String[] args) {
+        // @todo: Implement the Kafka producer
+    }
+
+    private static void printRecord(ProducerRecord record) {
+        System.out.println("Sent record:");
+        System.out.println("\tTopic = " + record.topic());
+        System.out.println("\tPartition = " + record.partition());
+        System.out.println("\tKey = " + record.key());
+        System.out.println("\tValue = " + record.value());
+    }
+}
