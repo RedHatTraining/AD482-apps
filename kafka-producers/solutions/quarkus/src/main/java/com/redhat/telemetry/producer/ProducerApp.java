794b8b244de9af8cc5b10cfe316b61b82eb8694c
Pablo Solar Vilariño <psolarvi@redhat.com>
2021-06-07 16:13:14 +0200

    Added solution for the Quarkus Kafka producer

---
+++ kafka-producers/solutions/quarkus/src/main/java/com/redhat/telemetry/producer/ProducerApp.java
@@ -0,0 +1,34 @@
+package com.redhat.telemetry.producer;
+
+import java.time.Duration;
+import java.util.Random;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.reactive.messaging.kafka.Record;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class ProducerApp {
+
+    private static final Logger LOG = Logger.getLogger(ProducerApp.class);
+
+    private final Random random = new Random();
+
+    // @todo: Implement the Kafka producer
+    @Outgoing("device-temperatures")
+    public Multi<Record<String, Integer>> generate() {
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+                .onOverflow().drop()
+                .map(tick -> {
+                    String currentDevice = "device-" + random.nextInt(10);
+                    int currentMeasure = random.nextInt(100);
+
+                    LOG.infov("Device ID: {0}, measure: {1}", currentDevice, currentMeasure);
+
+                    return Record.of(currentDevice, currentMeasure);
+                });
+    }
+}
