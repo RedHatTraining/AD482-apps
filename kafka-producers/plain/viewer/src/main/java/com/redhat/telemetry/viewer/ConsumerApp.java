a7ecdc87405fab4af483651587fd167d1e999f23
Pablo Solar Vilariño <60100327+psolarvi@users.noreply.github.com>
2021-06-07 13:06:05 +0200

    ge(ch02s08): Added plain Java producer with plain viewer (#3)

---
+++ kafka-producers/plain/viewer/src/main/java/com/redhat/telemetry/viewer/ConsumerApp.java
@@ -0,0 +1,54 @@
+package com.redhat.telemetry.viewer;
+
+import com.redhat.telemetry.producer.ProducerApp;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SslConfigs;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerApp
+{
+    public static Properties configureProperties() {
+        Properties producerProperties = ProducerApp.configureProperties();
+        Properties props = new Properties();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, producerProperties.getProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "total-connected-devices-consumer-group");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
+
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, producerProperties.getProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG));
+        props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, producerProperties.getProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG));
+        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, producerProperties.getProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG));
+        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, producerProperties.getProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG));
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, producerProperties.getProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG));
+
+        return props;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(configureProperties());
+
+        Consumer<Void,Integer> consumer = new KafkaConsumer<>(configureProperties());
+
+        consumer.subscribe(Collections.singletonList("total-connected-devices"));
+
+        while (true) {
+            System.out.println("Waiting for events...");
+
+            ConsumerRecords<Void, Integer> records = consumer.poll(Duration.ofMillis(10000));
+
+            for (ConsumerRecord<Void, Integer> record : records) {
+                System.out.println("Received total-connected-devices: " + record.value());
+            }
+        }
+    }
+}
---
+++ kafka-producers/solutions/plain/producer/src/main/java/com/redhat/telemetry/producer/ProducerApp.java
@@ -0,0 +1,76 @@
+package com.redhat.telemetry.producer;
+
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SslConfigs;
+
+public class ProducerApp {
+    public static Properties configureProperties() {
+        Properties props = new Properties();
+
+        // @todo: configure the bootstrap server
+        props.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "YOUR_KAFKA_BOOTSTRAP_HOST:YOUR_KAFKA_BOOTSTRAP_PORT"
+        );
+
+        // @todo: configure the key and value serializers
+        props.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringSerializer"
+        );
+        props.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.IntegerSerializer"
+        );
+
+        // @todo: configure the SSL connection
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        props.put(
+                SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
+                "ABSOLUTE_PATH_TO_WORKSPACE_FOLDER/keystore.jks"
+        );
+        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "password");
+        props.put(
+                SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                "ABSOLUTE_PATH_TO_WORKSPACE_FOLDER/truststore.jks"
+        );
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "password");
+
+        return props;
+    }
+
+    public static void main(String[] args) {
+        // @todo: Implement the Kafka producer
+        Random random = new Random();
+        Producer<Void,Integer> producer = new KafkaProducer<>(
+                configureProperties()
+        );
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<Void, Integer> record = new ProducerRecord<>(
+                    "total-connected-devices",
+                    random.nextInt(100)
+            );
+
+            producer.send(record);
+            printRecord(record);
+        }
+
+        producer.close();
+    }
+
+    private static void printRecord(ProducerRecord record) {
+        System.out.println("Sent record:");
+        System.out.println("\tTopic = " + record.topic());
+        System.out.println("\tPartition = " + record.partition());
+        System.out.println("\tKey = " + record.key());
+        System.out.println("\tValue = " + record.value());
+    }
+}
