2d19c0e039a8fba8f120cef8580d94978bcd9924
Pablo Solar Vilariño <60100327+psolarvi@users.noreply.github.com>
2021-06-08 17:29:47 +0200

    ge(ch02s08): Added Quarkus producer with python viewer  (#5)

---
+++ kafka-producers/README.md
@@ -0,0 +1,9 @@
+# AD482 - Kafka Producers
+
+This folder contains the applications used in the Kafka Producers section:
+
+* The `plain` folder contains a plain Java producer for the kafka producers guided exercise.
+
+* The `quarkus` folder contains a Quarkus producer for the kafka producers guided exercise.
+
+* The `solutions` folder contains the final solutions of the kafka producers guided exercise.
---
+++ kafka-producers/quarkus/src/main/java/com/redhat/telemetry/producer/ProducerApp.java
@@ -0,0 +1,22 @@
+package com.redhat.telemetry.producer;
+
+import java.time.Duration;
+import java.util.Random;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.reactive.messaging.kafka.Record;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class ProducerApp {
+
+    private static final Logger LOG = Logger.getLogger(ProducerApp.class);
+
+    private final Random random = new Random();
+
+    // @todo: Implement the Kafka producer
+
+}
