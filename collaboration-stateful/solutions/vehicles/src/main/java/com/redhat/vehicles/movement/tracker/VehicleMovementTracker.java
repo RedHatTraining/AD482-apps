4031f7a0081e956faa81e8210e9430df7af5769b
Jaime Ramírez <jaime.ramirez@redhat.com>
2021-07-23 14:10:43 +0200

    adjust solution

--- collaboration-stateful/solutions/vehicles/src/main/java/com/redhat/vehicles/movement/tracker/VehicleMovementTracker.java
+++ collaboration-stateful/solutions/vehicles/src/main/java/com/redhat/vehicles/movement/tracker/VehicleMovementTracker.java
@@ -4,21 +4,15 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
 import com.redhat.vehicles.movement.events.VehicleMoved;
-import com.redhat.vehicles.movement.events.VehicleRegistered;
 
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.GlobalKTable;
-import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KGroupedStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.state.KeyValueStore;
 import io.quarkus.kafka.client.serialization.ObjectMapperSerde;
