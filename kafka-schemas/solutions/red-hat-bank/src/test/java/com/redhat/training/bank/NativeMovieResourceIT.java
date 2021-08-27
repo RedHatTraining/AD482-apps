63b75eb66b5ecff02775cc82cfebd759359a58c3
Marek <mczernek@redhat.com>
2021-08-27 14:16:32 +0200

    Correct Schemas GE tests

--- kafka-schemas/solutions/red-hat-bank/src/test/java/com/redhat/training/bank/NativeMovieResourceIT.java
+++ kafka-schemas/solutions/red-hat-bank/src/test/java/com/redhat/training/bank/NativeMovieResourceIT.java
@@ -3,7 +3,7 @@ package com.redhat.training.bank;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeMovieResourceIT extends MovieResourceTest {
+public class NativeMovieResourceIT extends BankResourceTest {
 
     // Execute the same tests but in native mode.
 }
