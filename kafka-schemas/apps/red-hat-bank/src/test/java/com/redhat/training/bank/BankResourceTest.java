e75b1e4399baba961dd5d1ada46556ee297ebcb7
Marek Czernek <mczernek@redhat.com>
2021-08-27 22:31:45 +0200

    fix(ch02s12): Correct tests for the schema GE apps (#32)

--- kafka-schemas/apps/red-hat-bank/src/test/java/com/redhat/training/bank/BankResourceTest.java
+++ kafka-schemas/apps/red-hat-bank/src/test/java/com/redhat/training/bank/BankResourceTest.java
@@ -4,18 +4,18 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
 
 @QuarkusTest
-public class MovieResourceTest {
+public class BankResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/movies")
+          .when().get("/")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy Reactive"));
+             .body(containsStringIgnoringCase("Red Hat Training Bank"));
     }
 
 }
