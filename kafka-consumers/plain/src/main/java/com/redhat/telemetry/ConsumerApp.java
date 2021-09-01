0a6d4b27eba752d672b7ebb035d3ecc64bf5fb58
Enol Álvarez <ealvarez@redhat.com>
2021-09-01 15:36:42 +0200

    fix(ch02s10): Correct style

--- kafka-consumers/plain/src/main/java/com/redhat/telemetry/ConsumerApp.java
+++ kafka-consumers/plain/src/main/java/com/redhat/telemetry/ConsumerApp.java
@@ -23,8 +23,7 @@ public class ConsumerApp
         return props;
     }
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         // Create Kafka consumer here
     }
 }
