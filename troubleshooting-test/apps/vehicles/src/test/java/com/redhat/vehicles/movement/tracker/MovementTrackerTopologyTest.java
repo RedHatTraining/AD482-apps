2cd527bc0f89aef50e2981faa593c6e751dafad7
Jaime Ramirez <jaime.ramirez@redhat.com>
2021-09-20 17:40:36 +0200

    update and clean code

--- troubleshooting-test/apps/vehicles/src/test/java/com/redhat/vehicles/movement/tracker/MovementTrackerTopologyTest.java
+++ troubleshooting-test/apps/vehicles/src/test/java/com/redhat/vehicles/movement/tracker/MovementTrackerTopologyTest.java
@@ -12,10 +12,13 @@ import com.redhat.vehicles.events.VehicleMoved;
 import com.redhat.vehicles.events.VehicleRegistered;
 import com.redhat.vehicles.inventory.Vehicle;
 
+import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
 import org.apache.kafka.streams.TopologyTestDriver;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.test.TestRecord;
 
 
 
@@ -33,12 +36,14 @@ class MovementTrackerTopologyTest {
     TestInputTopic<Integer, VehicleMoved> vehicleMovedTopic;
     ObjectMapperSerde<VehicleMoved> vehicleMovedSerde;
     KeyValueStore<Integer, VehicleMetrics> vehicleMetricsStore;
+    TestOutputTopic<Integer, VehicleStatus> vehicleStatusTopic;
+    ObjectMapperSerde<VehicleStatus> vehicleStatusSerde;
 
     @BeforeEach
     public void setup() {
         // TODO: pass the VehicleMovementTracker topology to the test driver
 
-        // TODO: Create test topics
+        // TODO: Create test output topics
 
         // TODO: Create test stores
     }
@@ -51,5 +56,7 @@ class MovementTrackerTopologyTest {
     // TODO: test VehicleRegistered events create vehicles in the store
 
     // TODO: test VehicleMetrics counts VehicleMoved events for each vehicle
+
+    // TODO: test VehicleMoved and VehicleRegistered events are joined
 
 }
