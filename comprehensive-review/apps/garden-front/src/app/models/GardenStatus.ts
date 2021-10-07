76372d2d89cff23b89bb55b79bcde64571302017
Jaime Ramirez <jaime.ramirez@redhat.com>
2021-10-07 16:15:57 +0200

    initial commit for the frontend

---
+++ comprehensive-review/apps/garden-front/src/app/models/GardenStatus.ts
@@ -0,0 +1,8 @@
+export interface GardenStatus {
+    id: number,
+    name: string,
+    temperature: number,
+    garden: string,
+    sensorId: number,
+    lastUpdate: Date
+}
---
+++ comprehensive-review/apps/garden-front/src/app/models/SensorMeasurement.ts
@@ -0,0 +1,7 @@
+export interface SensorMeasurement {
+    type: string,
+    value: any,
+    garden: string,
+    sensorId: number,
+    timestamp: Date
+}
