784fa0ab7dbcc3a9e90fdd12758cb11c788a660d
Jaime Ramirez <jaime.ramirez@redhat.com>
2021-10-20 18:08:40 +0200

    add front and missing changes in back

--- comprehensive-review/apps/garden-back/src/main/java/com/redhat/training/gardens/event/LowTemperatureDetected.java
+++ comprehensive-review/apps/garden-back/src/main/java/com/redhat/training/gardens/event/LowTemperatureDetected.java
@@ -1,6 +1,6 @@
 package com.redhat.training.gardens.event;
 
-public class LowTemperatureDetected extends GardenEvent {
+public class LowTemperatureDetected extends GardenAlertEvent {
     public LowTemperatureDetected() {}
 
     public LowTemperatureDetected(String name, String gardenName, Integer sensorId, Double value, Long timestamp) {
